70cfef7590a5dd0f82cc54e1050eebe3e07e9324
muzzlol <muzxmmilkhxn@gmail.com>
2024-12-25 23:12:04 +0300

    placeholder data

---
+++ src/data/repositories.js
@@ -0,0 +1,51 @@
+export const repositories = [
+  {
+    name: "design-system",
+    visibility: "Public",
+    language: "React",
+    size: "7320 KB",
+    lastUpdate: "1 day ago",
+  },
+  {
+    name: "codeant-ci-app",
+    visibility: "Private",
+    language: "Javascript",
+    size: "5871 KB",
+    lastUpdate: "2 days ago",
+  },
+  {
+    name: "analytics-dashboard",
+    visibility: "Private",
+    language: "Python",
+    size: "4521 KB",
+    lastUpdate: "5 days ago",
+  },
+  {
+    name: "mobile-app",
+    visibility: "Public",
+    language: "Swift",
+    size: "3096 KB",
+    lastUpdate: "3 days ago",
+  },
+  {
+    name: "e-commerce-platform",
+    visibility: "Private",
+    language: "Java",
+    size: "6210 KB",
+    lastUpdate: "6 days ago",
+  },
+  {
+    name: "blog-website",
+    visibility: "Public",
+    language: "HTML/CSS",
+    size: "1876 KB",
+    lastUpdate: "4 days ago",
+  },
+  {
+    name: "social-network",
+    visibility: "Private",
+    language: "PHP",
+    size: "5432 KB",
+    lastUpdate: "7 days ago",
+  },
+];
